a17775e49fda5c6501e6ba09029d3e6af583e301
LeonidMashanov <len4ik272523@gmail.com>
2021-01-13 16:17:34 +0200

    create class ArrayPrinter and ArrayForPrint

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,74 @@
 package com.company;
 
+
+import java.util.Arrays;
+import java.util.Objects;
+
+
+class ArrayPrinter {
+    ArrayForPrint arrayForPrint;
+
+    public ArrayPrinter(ArrayForPrint arrayForPrint) {
+        this.arrayForPrint = arrayForPrint;
+    }
+
+    public void printArray() {
+
+        for (int i = 0; i < arrayForPrint.size(); i++) {
+            printElem(i);
+        }
+    }
+
+    protected void printElem(int elem) {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ArrayPrinter that = (ArrayPrinter) o;
+        return arrayForPrint.equals(that.arrayForPrint);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(arrayForPrint);
+    }
+
+    @Override
+    public String toString() {
+        return "ArrayPrinter{" +
+                "arrayForPrint=" + arrayForPrint +
+                '}';
+    }
+}
+
+class ArrayForPrint {
+    int[] array;
+
+    public ArrayForPrint(int[] array) {
+        this.array = array;
+    }
+
+    public int getArrayElement(int index) {
+        int value = array[index];
+        return value;
+    }
+
+    public int size() {
+        return array.length;
+    }
+
+
+
+}
+
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+
+
     }
 }
